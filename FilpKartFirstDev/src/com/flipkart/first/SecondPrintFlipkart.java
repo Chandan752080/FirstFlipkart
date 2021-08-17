516846175caaa9e6876e34e6626d8e0162d43154
Chandan Kumar Rout <chandan.kumar@s-force.org>
2021-08-17 23:01:48 +0530

    Again try to correct code

--- FilpKartFirstDev/src/com/flipkart/first/SecondPrintFlipkart.java
+++ FilpKartFirstDev/src/com/flipkart/first/SecondPrintFlipkart.java
@@ -3,6 +3,6 @@ package com.flipkart.first;
 public class SecondPrintFlipkart {
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.println("Print the flipkart Data");
+		System.out.println("Print the flipkart Data...");
 	}
 }
